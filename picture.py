441c1b664c93441bb0bf609097d936c1dd9d168d
nicolefeng2 <nicole.feng2@gmail.com>
2022-02-03 17:07:32 -0500

    stuff

--- picture.py
+++ picture.py
@@ -2,4 +2,11 @@ import cv2 as cv
 import numpy as np
 import sys
 import matplotlib
+import colordetect
 
+#general idea:
+# 1. use colordetect to detect 4 tape fingers
+# 2. use opencv hand gesture detection for L-frame shapes
+# 3. use gesture to trigger the screen cap 
+# 4. crop photo to fit into dimensions of hand frame
+# 5. export photo to output display

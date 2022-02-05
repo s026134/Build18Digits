df61a4dfeef62c3b4e4b82abdb0df0f255ef21a8
Your Name <s026134@students.lmsd.org>
2022-02-05 15:43:29 -0500

    starting on the digit count

--- HandGestures.py
+++ HandGestures.py
@@ -2,6 +2,7 @@ import cv2
 import time
 import os
 from cvzone.HandTrackingModule import HandDetector
+from matplotlib.pyplot import draw
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8, maxHands=4)
@@ -20,6 +21,7 @@ while True:
     cv2.putText(img, f'FPS: {int(fps)}', (500, 70), cv2.FONT_HERSHEY_PLAIN, 
     3,(255,0,0), 3)
 
+    tipIds = [4, 8, 12, 16, 20]
 
     if hands:
         # Hand 1
@@ -28,6 +30,8 @@ while True:
         bbox1 = hand1["bbox"]  # Bounding Box info x,y,w,h
         centerPoint1 = hand1["center"]  # center of the hand cx,cy
         handType1 = hand1["type"]  # Hand Type Left or Right
+
+        lmList = detector.findPosition(img, draw = False)
  
         # print(len(lmList1),lmList1)
         # print(bbox1)
@@ -35,7 +39,27 @@ while True:
         fingers1 = detector.fingersUp(hand1)
         #length, info, img = detector.findDistance(lmList1[8], lmList1[12], img) # with draw
         #length, info = detector.findDistance(lmList1[8], lmList1[12])  # no draw
- 
+
+        #Finger Counting code
+        if len(lmList) != 0:
+            fingers = []
+
+            #Thumb
+            if lmList[tipIds[0]][1] > lmList[tipIds[0]-1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+            for id in range(1,5):
+                if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
+                   fingers.append(1)
+                else:
+                    fingers.append(0)
+        
+            print(f'One Hand: {fingers}')
+            
+        
+
  
         if len(hands) == 2:
             hand2 = hands[1]
@@ -43,6 +67,18 @@ while True:
             bbox2 = hand2["bbox"]  # Bounding Box info x,y,w,h
             centerPoint2 = hand2["center"]  # center of the hand cx,cy
             handType2 = hand2["type"]  # Hand Type Left or Right
+
+            lmList2Hands = detector.findPosition(img, draw = False)
+            #Finger Counting code
+            if len(lmList2Hands) != 0:
+                fingers = []
+                for id in range(0,5):
+                    if lmList2Hands[tipIds[id]][2] < lmList2Hands[tipIds[id]-2][2]:
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
+            
+                print(f'Two Hands: {fingers}')
  
             fingers2 = detector.fingersUp(hand2)
             # print(fingers1, fingers2)
@@ -54,7 +90,7 @@ while True:
             x2,y2 = centerPoint2
 
 
-    cv2.imshow("Image", cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE))
+    cv2.imshow("Image", img)
     cv2.waitKey(1)
 
     if cv2.waitKey(30) == ord('q'):
